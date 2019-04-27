7b6014bcffe204ddfc4aab3d46b75b38a78c725d
GBressler <gregbressler@gmail.com>
2019-04-27 01:55:35 -0500

    Built Inventory Form and Menu view

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,48 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish"
 
 
 class App extends React.Component {
+  state= {
+   fishes: {},
+   order: {}
+  };
+
+  addFish = fish => {
+    console.log("Adding fishes");
+     //1. Take a copy of existing state
+     const fishes = {...this.state.fishes};
+     //2.Add new fish to variable
+     fishes[`fish${Date.now()}`] = fish;
+     //3. Set the new fishes to state
+     this.setState({
+       fishes: fishes
+     })
+  };
+
+  loadSampleFishes = () => {
+    //alert('Loading');
+    this.setState({fishes: sampleFishes})
+
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Catch of the Day" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+          </ul>
         </div>
+       
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} 
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
